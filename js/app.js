5d282fbceeb7fa319ec0cf49159ffe0e5df52447
LuisJavier Hernandez <luisjavierhernandez150@gmail.com>
2022-09-27 13:05:33 -0500

    finalizando

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//variables
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const listaCursos = document.querySelector('#lista-cursos');
+const vaciarCarrito = document.querySelector('#vaciar -carrito');
+
+cargarEventListener();
+function cargarEventListener(){
+    //
+    listaCursos.addEventListener('click',agregarCurso);
+}
+
+//funciones
+function agregarCurso(e){
+    e.preventDefault();
+    if (e.target.classList.contains('agregar-carrito'))
+    console.log(e.target);
+}
